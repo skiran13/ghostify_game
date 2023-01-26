2c2439f37514648db5b5d495e86af11c969bc2ef
Surya Kiran Suresh <skiran13@gmail.com>
2023-01-26 18:44:46 -0500

    Refactor without Fragments

--- app/src/main/java/com/example/game/MainMenuActivity.kt
+++ app/src/main/java/com/example/game/MainMenuActivity.kt
@@ -5,19 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class SplashScreenActivity : AppCompatActivity() {
+class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_main_menu)
 
-
-        val button : Button = findViewById(R.id.launch_game)
+        val button : Button = findViewById(R.id.go_back_button)
         button.setOnClickListener{
-            val i = Intent(this, MainMenuActivity::class.java)
+            val i = Intent(this, SplashScreenActivity::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out,)
             finish()
         }
-
     }
 }
