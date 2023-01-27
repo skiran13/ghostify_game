681fb3993a6bfb8aba189f7f8b7f540495bbdfe3
Surya Kiran Suresh <skiran13@gmail.com>
2023-01-26 19:34:59 -0500

    New Game activity

--- app/src/main/java/com/example/game/MainMenuActivity.kt
+++ app/src/main/java/com/example/game/MainMenuActivity.kt
@@ -10,12 +10,18 @@ class MainMenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
 
-        val button : Button = findViewById(R.id.go_back_button)
-        button.setOnClickListener{
+        val go_back : Button = findViewById(R.id.go_back_button)
+        go_back.setOnClickListener{
             val i = Intent(this, SplashScreenActivity::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out,)
             finish()
+        }
+
+        val new_game : Button = findViewById(R.id.new_game_button)
+        new_game.setOnClickListener{
+            startActivity(Intent(this, NewGameActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out,)
         }
     }
 }
